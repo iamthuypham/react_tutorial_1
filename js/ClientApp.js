c7fa6e9d19b090e6bfc57d4633c5d5e4a915af22
Thuy <tpham045@hotmail.com>
2016-12-27 21:09:22 +0000

    Use standard

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,31 +1,29 @@
- // Global var
- var div = React.DOM.div
- var h1 = React.DOM.h1
+/* global React ReactDOM */
 
- //  Make our own component like div and h1
- var MyTitle = React.createClass({
-     render() {
-         return (
-             div(null,
-                 h1({style: {color: this.props.color}},this.props.title //using props to pass manual infomation
-                     
-                 )
-             )
-         )
-     }
- })
+// Global var
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
- var MyTitleFact = React.createFactory(MyTitle)
- var ce = React.createElement
+//  Make our own component like div and h1
+var MyTitle = React.createClass({
+  render () {
+    return (
+            div(null, h1({style: {color: this.props.color}}, this.props.title
+                )
+            )
+    )
+  }
+})
 
- var MyFirstComponent = (
-     div(null,
-         MyTitleFact({title: 'Props are great'}),
-         ce(MyTitle, {title: 'Props second', color: 'red'}),
-         React.createElement(MyTitle, {title: 'Passing props here'})
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
 
-     )
- )
+var MyFirstComponent = (
+    div(null,
+        MyTitleFact({title: 'Props are great'}),
+        ce(MyTitle, {title: 'Props second', color: 'red'}),
+        React.createElement(MyTitle, {title: 'Passing props here'})
+    )
+)
 
- ReactDOM.render(MyFirstComponent, document.getElementById('app'))
- 
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
