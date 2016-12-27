a8caf825ee61cc62af9e9e7757e75ee8ea9071bd
Thuy <tpham045@hotmail.com>
2016-12-27 20:53:31 +0000

    Push to master branch

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,31 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router'
-import App from './App'
+ // Global var
+ var div = React.DOM.div
+ var h1 = React.DOM.h1
 
-render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'))
+ //  Make our own component like div and h1
+ var MyTitle = React.createClass({
+     render() {
+         return (
+             div(null,
+                 h1({style: {color: this.props.color}},this.props.title //using props to pass manual infomation
+                     
+                 )
+             )
+         )
+     }
+ })
+
+ var MyTitleFact = React.createFactory(MyTitle)
+ var ce = React.createElement
+
+ var MyFirstComponent = (
+     div(null,
+         MyTitleFact({title: 'Props are great'}),
+         ce(MyTitle, {title: 'Props second', color: 'red'}),
+         React.createElement(MyTitle, {title: 'Passing props here'})
+
+     )
+ )
+
+ ReactDOM.render(MyFirstComponent, document.getElementById('app'))
+ 
